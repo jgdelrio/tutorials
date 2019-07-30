937b02862ad71f42f04e54ea9d6ac25cf1cf75eb
jdelrioLG <jdelrio@libertyglobal.com>
2019-07-30 12:58:33 +0100

    Added 10_Text_Classification with the basics Signed-off-by: jdelrioLG <jdelrio@libertyglobal.com>

--- src/text_func.py
+++ src/text_func.py
@@ -1,11 +1,34 @@
 import re
 import unicodedata
 
+import nltk
+from nltk.tokenize import ToktokTokenizer
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.corpus import wordnet
+import spacy
+from textblob import TextBlob
+
 
 SPECIAL_CHARS = "`1234567890-=~@#$%^&*()_+[!{;”:\’'><.,/?”}]"
 
+stop_word_list = stopwords.words('english')
+token = ToktokTokenizer()
+lemma = WordNetLemmatizer()
 
-def clean_text(text):
+# Dictionary for the transformation from wordnet tags to lemmatize characters
+tag_dict_wordnet = {"J": wordnet.ADJ,
+                    "N": wordnet.NOUN,
+                    "V": wordnet.VERB,
+                    "R": wordnet.ADV}
+
+tag_dict_textblob = {"J": 'a',
+                      "N": 'n',
+                      "V": 'v',
+                      "R": 'r'}
+
+
+def text_replacements(text):
     text = text.lower()
     text = text.replace(r"i'm ", "i am ")
     text = text.replace(r"he's ", 'he is ')
@@ -65,4 +88,50 @@ def remove_special(text):
         if w in SPECIAL_CHARS:
             text = text.replace(w, ' ')
     return text
+
+
+def clean_text(text):
+    text = remove_special(remove_accents(text_replacements(text)))
+    return text
+
+
+def get_wordnet_pos(word):
+    """Map POS tag to first character for lemmatize()"""
+    tag = nltk.pos_tag([word])[0][1][0].upper()
+
+    return tag_dict_wordnet.get(tag, wordnet.NOUN)
+
+
+def get_textblob_pos(word):
+    """Map POS tag to first character for lemmatize()"""
+    tag = nltk.pos_tag([word])[0][1][0].upper()
+
+    return tag_dict_textblob.get(tag, wordnet.NOUN)
+
+
+def remove_stop_words_and_lemmatize(text, token_output=False):
+    word_list = [x.strip() for x in token.tokenize(text)]
+    output_list = [lemma.lemmatize(x, get_wordnet_pos(x)) for x in word_list if not x in stop_word_list]
+
+    if token_output:
+        return output_list
+    else:
+        return " ".join(output_list)
+
+
+def lemmatize_doc(text, lemmatizer='nltk'):
+    if lemmatizer == 'nltk':
+        lematized_doc = [lemma.lemmatize(w, get_wordnet_pos(w)) for w in nltk.word_tokenize(text)]
+        return ' '.join(lematized_doc)
+
+    elif lemmatizer == 'spacy':
+        nlp = spacy.load('en', disable=['parser', 'ner'])
+        doc = nlp(text)
+        return " ".join([token.lemma_ for token in doc])
+
+    elif lemmatizer == 'textblob':
+        sentence = TextBlob(text)
+        words_and_tags = [(w, tag_dict_textblob.get(pos[0], 'n')) for w, pos in sentence.tags]
+        lemmatized_list = [wd.lemmatize(tag) for wd, tag in words_and_tags]
+        return " ".join(lemmatized_list)
 
